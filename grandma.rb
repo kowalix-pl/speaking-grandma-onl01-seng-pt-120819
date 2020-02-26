55ca3b85eba4b0739d655aad0e2c94b7150ef25b
kowalix007 L.K <kowalix007@gmail.com>
2020-02-26 12:05:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.upcase
      "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == !phrase.upcase
+  elsif phrase !== phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
 end 
 end 
