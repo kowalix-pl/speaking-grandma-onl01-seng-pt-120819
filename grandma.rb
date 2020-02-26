22a6a04987aedfebbb2c9173970611312efdb92c
kowalix007 L.K <kowalix007@gmail.com>
2020-02-26 12:17:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,10 +17,9 @@ def speak_to_grandma (phrase)
      "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
   else phrase == "WHAT?"
      "NO, NOT SINCE 1938!"
-  
 end 
 end 
